7a0d6be2991575d35c3a1149a1ec9fff3d8c2fc1
Rodrigo Sanches Ferreira <rodrigof@tecgraf.puc-rio.br>
2017-05-25 17:37:50 -0300

    Auth adjusts + App Grid Component

---
+++ app/firebase-config.ts
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyAwqLT6tZ9eKRATdUqF8F9KztqtWw3ADN8",
+    authDomain: "test-760a6.firebaseapp.com",
+    databaseURL: "https://test-760a6.firebaseio.com",
+    projectId: "test-760a6",
+    storageBucket: "test-760a6.appspot.com",
+    messagingSenderId: "956449971829"
+};
+
+export default config;
--- app/main.ts
+++ app/main.ts
@@ -1,37 +1,24 @@
 import * as firebase from "firebase";
 import stateEngine from "./state-engine";
 import App from "./app";
+import config from "./firebase-config";
 
 new App(document.body, "app");
 
-const config = {
-    apiKey: "AIzaSyAwqLT6tZ9eKRATdUqF8F9KztqtWw3ADN8",
-    authDomain: "test-760a6.firebaseapp.com",
-    databaseURL: "https://test-760a6.firebaseio.com",
-    projectId: "test-760a6",
-    storageBucket: "test-760a6.appspot.com",
-    messagingSenderId: "956449971829"
-};
-
 firebase.initializeApp(config);
 
-firebase.auth().getRedirectResult().then((result) => {
-    if (result.credential) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // ...
-        console.log(token);
-    }
-    // The signed-in user info.
-    console.log(result.user);
-    stateEngine.set("user", result.user);
-}).catch((error) => {
-    // Handle Errors here.
-    //var errorCode = error.code;
-    //var errorMessage = error.message;
-    // The email of the user's account used.
-    //var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    //var credential = error.credential;
-    // ...
+const user = firebase.auth().currentUser;
+
+if (user) {
+    stateEngine.set("user", user);
+} else {
+    stateEngine.set("user", null);
+}
+
+firebase.auth().onAuthStateChanged((user:any) =>{
+  if (user) {
+    stateEngine.set("user", user);
+  } else {
+    stateEngine.set("user", null);
+  }
 });
